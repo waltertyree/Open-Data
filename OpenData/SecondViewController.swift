2d3c8b00fc5d83c0b4949a91ffce96feaf23d8a7
Walter Tyree <walter@tyreeapps.com>
2016-03-17 07:54:07 -0400

    Initial Commit

---
+++ OpenData/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OpenData
+//
+//  Created by Walter Tyree on 3/17/16.
+//  Copyright © 2016 Tyree Apps, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
